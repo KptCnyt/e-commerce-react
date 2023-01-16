e72b19c21d9d4ea26a26b083c5db80d896747d41
Cuneyt Turker <cuneytturker35@gmail.com>
2023-01-16 22:53:12 +0000

    navbar component implemented

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { HamburgerMenu } from "./HamburgerMenu";
+
+export const NavBar = () => {
+    return (
+        <div>
+            <HamburgerMenu/>
+        </div>
+    )
+}
